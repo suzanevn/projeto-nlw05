5c202ebd124ba993be586fdbe4f69b965ec3d233
Suzane Nascimento <suzane.nascimento@sienacompany.com>
2021-04-19 13:20:41 -0300

    comit inicial

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({message: "ola nlw 05"})
+});
+
+app.post("/users", (request, response) => {
+    return response.json({ message: "usuario salvo com sucesso!"});
+})
+
+app.listen(3333, ()=>console.log("server rodando porta 3333"));
