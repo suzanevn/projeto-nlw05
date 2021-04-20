4c8d33819a611c8053f418607dfc7016fa15cb5c
Suzane Nascimento <suzane.nascimento@sienacompany.com>
2021-04-20 11:38:49 -0300

    aula 2 ainda, faltou routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,12 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsController } from "./controllers/SettingsController";
 
 const routes = Router();
 
-/**
- * Tipos de parametros
- * Routes Params => Parametros de rotas
- * Query Params => Filtros e buscas, vem depois da rota ...?id/teste...
- * Body params => {...}
- */
+const settingsController = new SettingsController();
 
 routes.post("/settings", async (request, response) => {
-    const { chat, username } = request.body;
-    const settingsRepository = getCustomRepository(SettingsRepository);
-    const settings = settingsRepository.create({
-        chat,
-        username
-    });
-    await settingsRepository.save(settings);
-    return response.json(settings);
+    
 })
 
 export { routes }
